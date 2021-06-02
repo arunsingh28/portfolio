adf011bc33e91e988af2b8a89b1c8684a24ff2a0
Arun Singh <arunsingh28aug.as@gmail.com>
2021-06-03 01:30:12 +0530

    day off

--- src/components/screen/Home.js
+++ src/components/screen/Home.js
@@ -4,6 +4,7 @@ import instagram from '../util/logo/instagram.gif'
 import github from '../util/logo/github.svg'
 import linkedin from '../util/logo/linkedin.gif'
 import facebook from '../util/logo/facebook.gif'
+import profile from '../util/logo/profile.JPG'
 
 const Home = () => {
     return (
@@ -11,20 +12,31 @@ const Home = () => {
             <div className="bg_cover"></div>
             <div className="card_container">
                 <div className="icard">
-                <img src="https://static.wixstatic.com/media/2e2a49_bd3a1599bc1b4265a01b2754b99b8536~mv2_d_5760_3840_s_4_2.jpeg/v1/crop/x_2703,y_43,w_2199,h_2205/fill/w_410,h_410,al_c,q_80,usm_0.66_1.00_0.01/AdobeStock_230407429.webp" alt="profile" />
-                <h3>Arun <br /> Singh</h3>
-                <span></span>
-                <p>Mern stack developer</p>
-                <div className="social_Link">
-                    <img src={instagram} alt="instagram" />
-                    <img src={github} alt="gihub" />
-                    <img src={facebook} alt="facebook" />
-                    <img src={linkedin} alt="linkedin" />
-                </div>
+                    <img src={profile} alt="profile" />
+                    <h3>Arun <br /> Singh</h3>
+                    <span></span>
+                    <p>Mern stack developer</p>
+                    <div className="social_Link">
+                        <img src={instagram} alt="instagram" />
+                        <img src={github} alt="gihub" />
+                        <img src={facebook} alt="facebook" />
+                        <img src={linkedin} alt="linkedin" />
+                    </div>
                 </div>
 
                 <div className="content">
                     <h1>Hello world</h1>
+                    <p>Here's who i am & what i do</p>
+
+                    <div className="btn_container">
+                        <button>Resume</button>
+                        <button>Projects</button>
+                    </div>
+
+                    <p className="bio">I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.
+                    <br/>  <br/>  <br/>
+                    I’m a great place for you to tell a story and let your users know a little more about you.
+                    </p>
                 </div>
             </div>
         </div>
